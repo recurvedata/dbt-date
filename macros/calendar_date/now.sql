af08b9286307c7439a6945057317509789e8545d
JingFeng.Ren <jingfeng.ren@recurvedata.com>
2024-12-05 11:23:33 +0800

    modifications in 'now.sql'

--- macros/calendar_date/now.sql
+++ macros/calendar_date/now.sql
@@ -2,10 +2,10 @@
 {{ adapter.dispatch('now', 'dbt_date') (tz) }}
 {%- endmacro -%}
 
-{%- macro default__now(tz=None) -%}
+{%- macro default__now(tz) -%}
 {{ dbt_date.convert_timezone(dbt.current_timestamp(), tz) }}
 {%- endmacro -%}
 
-{%- macro doris__now(tz=None) -%}
-dbt.current_timestamp()
+{%- macro doris__now(tz) -%}
+{{ dbt.current_timestamp() }}
 {%- endmacro -%}
